d9d717c14253a92f40d676e454c1a7ab7575bfa7
sandonemaki <kamuramaki@gmail.com>
2022-10-12 12:11:22 +0900

    不要なコメントを消した

--- lib/effects.rb
+++ lib/effects.rb
@@ -98,23 +98,3 @@ end
 #		effect = Effects::Pitch_shift.new(-1)
 #		# assert_equal 'HAL',
 #		effect.call('IBM')
-
-
-# def self.reverse
-#   ->(words) do
-#     words.split(' ').map(&:reverse).join(' ')
-#   end
-# end
-
-# def self.echo(rate)
-#   ->(words) do
-#     words.each_char.map { |c| c == ' ' ? c : c*rate }.join
-#   end
-# end
-
-# def self.loud(level)
-#   ->(words) do
-#     words.split(' ').map {|word| word.upcase + '!' * level}.join(' ')
-#   end
-# end
-
